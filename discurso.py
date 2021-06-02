2097f8add8958353f746da866b5a3699bae77efa
Joselo <impacevanend@gmail.com>
2021-06-02 14:14:29 -0500

    discurso

---
+++ discurso.py
@@ -0,0 +1,36 @@
+import random
+
+lambetazo = ["Queridos","Apreciados","Distinguidos","Honorables","Estimados","Respetados"]
+
+pntls_marranos = ["compatriostas","conciudadanos","amigos","coterraneos","compartidarios","electores"]
+
+condicion = ["en mi gobierno","con su apoyo","siendo elegido","con su ayuda","si me siguen","durante mi mandato"]
+
+compromiso = ["voy a derrotar","venceré","eliminaré","acabaré","lucharé contra","combatiré"]
+
+ilsn_guerrerista = ["la violencia y","la delincuencia y","la corrupción y","la inflación y","la pobreza y","el desplazamiento y"]
+
+promesa = ["trabajaré por","garantizaré","protegeré","velaré por","promoveré","defenderé"]
+
+bnfcio_populista = ["la educación","el empleo","la seguridad","la paz","la igualdad","la salud"]
+
+dpndendo_cntdad_votos = ["del país","de la ciudad","de la comunidad","de la población","para toda la gente","de cada colombiano"]
+
+
+lambetazo_1 = random.choice(lambetazo)
+
+pntls_marranos_2 = random.choice(pntls_marranos)
+
+condicion_3 = random.choice(condicion)
+
+compromiso_4 = random.choice(compromiso)
+
+ilsn_guerrerista_5 = random.choice(ilsn_guerrerista)
+
+promesa_6 = random.choice(promesa)
+
+bnfcio_populista_7 = random.choice(bnfcio_populista)
+
+dpndendo_cntdad_votos_8 = random.choice(dpndendo_cntdad_votos)
+
+print("Discurso: "+lambetazo_1+" "+" "+pntls_marranos_2+" "+condicion_3+" "+compromiso_4+" "+ilsn_guerrerista_5+" "+promesa_6+" "+bnfcio_populista_7+" "+dpndendo_cntdad_votos_8)
