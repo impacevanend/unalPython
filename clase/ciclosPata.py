836904ca76b9462dc7a5206fd147e45085dbe1f8
Joselo <impacevanend@gmail.com>
2021-06-15 16:59:34 -0500

    cambios en for

---
+++ clase/ciclosPata.py
@@ -0,0 +1,64 @@
+
+#*Forma compacta de escribir un ciclo mientras (while)
+#*para iterar sobre los elementos de una colección de elementos.
+
+#*“Para cada elemento
+#* en la colecci ́on realice las instrucciones en el <bloque>”.
+
+lista = ["Tomate de árbol", "Maracuyá", "Guayaba", "Lulo", "Granadilla"]
+
+#*Forzando la detención del recorrido una lista I con break
+"""
+for i in lista:
+    
+    print(i)
+    
+    if i == "Guayaba" :
+        
+        break
+        
+
+"""
+    
+    
+#*Colecciones range y los ciclos para
+"""
+for i in range(-5,6, 1):
+
+    print(i)
+    
+"""
+
+#*Imprimir un listado con los número del 1 al 100 cada uno con su respectivo cuadrado.
+
+"""
+for i in range(100+1):
+    print(f'número: {i} cuadrado: {i**2} ')
+"""
+
+#*Imprimir un listado con los n ́umeros impares desde 1 hasta 999 y
+#*seguidamente otro listado con los n ́umeros pares desde 2 hasta 1000.
+
+"""
+for i in range(100+1):
+    
+    if i% 2 == 0:
+        print(f'Número par: {i}')
+    
+    else:
+        print(f'Número impar: {i}')
+"""
+
+#*Imprimir los n ́umeros pares en forma descendente hasta 2 que son
+#*menores o iguales a un n ́umero natural n ≥ 2 dado.
+"""   
+for i in range(10, 0, -1):
+    
+    if i%2 == 0:
+        print(f'número par: {i}')
+
+"""
+    
+
+
+    
