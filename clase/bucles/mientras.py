f88bf78c8788b6699d8a358c94cc2c92e6c0eb69
Joselo <impacevanend@gmail.com>
2021-06-16 12:21:59 -0500

    profundización en bucles

---
+++ clase/bucles/mientras.py
@@ -0,0 +1,31 @@
+"""
+
+if 2 < 3:
+    print("Hola")
+    
+
+while 2 < 3:
+    
+    print("Hola")
+"""
+
+"""
+*Programa que muestre en pantalla una secuencia de números del 1 al 10
+"""
+"""
+i = 1
+
+while i <= 10:
+    print(i)
+    i +=1
+"""
+"""
+*Programa que muestre en pantalla una secuencia de número del 20 al 0
+"""
+
+i = 20
+
+while i >= 0:
+
+    print(i)
+    i -= 1
