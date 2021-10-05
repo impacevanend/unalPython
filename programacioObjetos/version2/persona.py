5d7ecf261c712cc217f92809d86065fdae16092d
Joselo <impacevanend@gmail.com>
2021-10-05 12:58:57 -0500

    Se agrega versión multi jugador

--- programacioObjetos/version2/persona.py
+++ programacioObjetos/version2/persona.py
@@ -4,9 +4,10 @@ class Persona:
     def __init__(self, nombre):
         self.nombre = nombre
         self.calle = 1
+        self.energia = 15
         
     def situacion(self):
-        return "{}, estás en la calles {} ".format(self.nombre, self.calle)
+        return self.nombre, self.calle, self.energia
         
     def moverse(self, velocidad):
         if velocidad == '1':
@@ -15,7 +16,7 @@ class Persona:
             self.calle += 2
         else:
             self.calle += 3
-
+        self.energia -= 1
 
 jose = Persona("Jose")
 juan = Persona("Juan")
--- programacioObjetos/version2/principal.py
+++ programacioObjetos/version2/principal.py
@@ -13,61 +13,79 @@ print(" Los zombis avanzan 1, 2 ó 3 calles.")
 print(" Tú puedes avanzar 1, 2 o 3 calles.")
 print()
 
-nombre = input(" ¿Estás preparado? Cuál es tu nombre: ").capitalize()
-
-
-jugador = Persona(nombre)
+numero = ""
+while numero not in ("1","2","3","4"):
+    numero = input(" Número de jugadores (1/4): ")
+    
+jugadores = []
+for i in range(1,int(numero)+1):
+    nombre = input(" Nombre del "+ str(i) + "° jugador: ").capitalize()
+    jugador = Persona(nombre)
+    jugadores.append(jugador)
     
 horda = []
-
 for i in range(10):
-    
-    z = Zombi()
-    
+    z=Zombi()
     horda.append(z)
 
 
-while True:
+while len(jugadores) > 0:
     os.system("cls")
     
-    print(jugador.situacion())
-
+    print(" NOMBRE - CALLE - ENERGÍA")
+    print(" ------------------------")
+    for jugador in jugadores:
+        nom, cal, ene = jugador.situacion()
+        print("{:8} -  {:2}      {:2}".format(nom, cal, ene))
+    print()
     
     calles = []
     for zombi in horda:
         calles.append(zombi.calle)
-        
-    calles.sort()
-    print("Hay zombis en las calles:")
     
+    calles.sort()
+    print(" Hay zombis en las calles: ")
+    print()
+    print(" ", end="")
     for elemento in calles:
         print(elemento, end=" ")
     print()
     print()
     
-    if jugador.calle > 40:
-        print(" No te ha visto ningún zombi.")
-        print(" Te has librado de ser comido.")
-        print()
-        break
+    ganadores = []
+    for jugador in jugadores:
+        if jugador.calle >= 40:
+            ganadores.append(jugador)
+    if len(ganadores)>0:
+        for jugador in ganadores:
+            print(" {}, has escapado de los zombis.".format(jugador.nombre))
+            print(" Has ganado la partida.")
+            print()
+            break
     
-    comido = False
-    for zombi in horda:
-        if zombi.calle == jugador.calle:
-            comido = True
-    
-    if comido:
-        print( "Un zombi te acaba de ver.")
-        print( " Te va a comer. Se acabó el juego.")
-        print()
-        break
-    
-    velocidad = ""
-    while velocidad not in ("1", "2", "3"):
-        velocidad = input(" Cuánto quieres correr (1/2/3): ")
-    jugador.moverse(velocidad)
-    
+    for jugador in list(jugadores):
+        if jugador.energia <= 0:
+            print(" {}, has perdido toda la energía. Estás comido.".format(jugador.nombre))
+            jugadores.remove(jugador)
+            
+    for jugador in list(jugadores):
+        for zombi in horda:
+            if zombi.calle == jugador.calle:
+                if jugador in jugadores:
+                    print(" {}, un zombi te ha visto. Has perdido".format(jugador.nombre))
+                    jugadores.remove(jugador)
+                    
+    print()
+    for jugador in jugadores:
+        velocidad = ""
+        while velocidad not in("1","2","3"):
+            velocidad = input(" {}, cuánto quieres correr (1/2/3)".format(jugador.nombre))
+        jugador.moverse(velocidad)
+        
     for z in list(horda):
         z.moverse()
         if z.no_visible():
             horda.remove(z)
+else:
+    print(" Todos han sido comidos. No hay ganadores.")
+    print()
